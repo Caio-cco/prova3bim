0c51d8c59320e2284fda5bf2bdcc677ee0913a87
Caio Sousa Mello <cmcaiomello@gmail.com>
2025-09-16 17:17:47 -0300

    permissão implement

--- prova3bim/src/controller/salaPermissaoController.js
+++ prova3bim/src/controller/salaPermissaoController.js
@@ -22,15 +22,16 @@ endpoints.post('/sala/:sala/entrar', autenticador, async (req, resp) => {
 
 endpoints.post('/sala/:sala/aprovar/:usuario', autenticador, async (req, resp) => {
        
-    const salaid = req.params.sala
-    const aprovado = req.params.aprovar
-    let usuarioLogadoId = req.user.id
+    const salaid = req.params.sala;
+    const usuarioId = req.params.usuario;
+    const usuarioLogadoId = req.user.id;
 
-    if(!aprovado){
-        
-    }
-    if(aprovado === true){
-        
+    const aprovado = await salaPermissaoRepo.aprovarPermissao(salaid, usuarioId);
+
+    if (aprovado) {
+        resp.send({ message: "Usuário aprovado com sucesso." });
+    } else {
+        resp.status(404).send({ error: "Permissão não encontrada ou já aprovada." });
     }
     
 
--- prova3bim/src/repository/salaPermissaoRepository.js
+++ prova3bim/src/repository/salaPermissaoRepository.js
@@ -12,22 +12,21 @@ export async function inserirPermissao(salaId, usuarioId, aprovado) {
 
 
 export async function aprovarPermissao(salaId, usuarioId) {
-    const comando =`
-      update salaPermissao
-         set aprovado = TRUE
-         where sala_id and usuario_id = ?;
-    `
-
-    const [info] = await connection.query(comando, [salaId, usuarioId])
+   const comando = `
+        UPDATE salaPermissao
+            SET aprovado = TRUE
+        WHERE sala_id = ? AND usuario_id = ?;
+        `
+const [info] = await connection.query(comando, [salaId, usuarioId]);
 
 }
 
 
 export async function verificarPermissaoSala(salaId, usuarioId) {
     const comando = `
-    SELECT id 
-        FROM sala 
-        WHERE sala_id = ? AND usuario_id = ? AND aprovado = TRUE;
-    `
+            SELECT id 
+                FROM salaPermissao 
+            WHERE sala_id = ? AND usuario_id = ? AND aprovado = TRUE;
+            `
     const [info] = await connection.query(comando, [salaId, usuarioId])
 }
